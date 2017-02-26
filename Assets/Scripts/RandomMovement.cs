c052a9af8c7cd6831a0221145bb06f96f4cd7eec
MaxDeBenedetti <modsme@gmail.com>
2017-02-25 19:46:26 -0600

    I solved movement

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -22,33 +22,41 @@ public class RandomMovement : MonoBehaviour {
     {
         if(screenSpace.size == Rect.zero.size)
         {
-            screenSpace = Camera.main.pixelRect;
+            Transform t = GameObject.Find("PetBounds").GetComponent<Transform>();
+            screenSpace = new Rect(new Vector2(t.position.x - t.localScale.x/2, t.position.y - t.localScale.y / 2), t.localScale);
+            Debug.Log(screenSpace);
         }
     }
- 
- void Update()
+
+
+    void Update()
     {
-        // change to random direction at random intervals
-        if (Time.time >= tChange)
-        {
-            randomX = Random.Range(-2.0f, 2.0f); // with float parameters, a random float
-            randomY = Random.Range(-2.0f, 2.0f); //  between -2.0 and 2.0 is returned
-                                               // set a random interval between 0.5 and 1.5
-            tChange = Time.time + Random.Range(minTimeToTurn, maxTimeToTurn);
-        }
-        transform.Translate(new Vector3(randomX, randomY, 0) * moveSpeed * Time.deltaTime);
-        // if object reached any border, revert the appropriate direction
-        if (transform.position.x >= screenSpace.xMax || transform.position.x <= screenSpace.xMin)
-        {
-            randomX = -randomX;
-        }
-        if (transform.position.y >= screenSpace.yMax || transform.position.y <= screenSpace.yMin)
-        {
-            randomY = -randomY;
-        }
-        // make sure the position is inside the borders
-        transform.position = new Vector3( Mathf.Clamp(transform.position.x, screenSpace.xMin, screenSpace.xMax), 
-            Mathf.Clamp(transform.position.y, screenSpace.yMin, screenSpace.yMax));
+
+
+
+            // change to random direction at random intervals
+            if (Time.time >= tChange)
+            {
+
+                randomX = Random.Range(-2.0f, 2.0f); // with float parameters, a random float
+                randomY = Random.Range(-2.0f, 2.0f); //  between -2.0 and 2.0 is returned
+                                                     // set a random interval between 0.5 and 1.5
+                tChange = Time.time + Random.Range(minTimeToTurn, maxTimeToTurn);
+            }
+            transform.Translate(new Vector3(randomX, randomY, 0) * moveSpeed * Time.deltaTime);
+            // if object reached any border, revert the appropriate direction
+            if (transform.position.x >= screenSpace.xMax || transform.position.x <= screenSpace.xMin)
+            {
+                randomX = -randomX;
+            }
+            if (transform.position.y >= screenSpace.yMax || transform.position.y <= screenSpace.yMin)
+            {
+                randomY = -randomY;
+            }
+            // make sure the position is inside the borders
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, screenSpace.xMin, screenSpace.xMax),
+                Mathf.Clamp(transform.position.y, screenSpace.yMin, screenSpace.yMax));
+        
     }
 
  
